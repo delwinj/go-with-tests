0a51fadd8eb2d6cade96c0159c9eee7d79827e12
Delwin <thekkinkad.d@husky.neu.edu>
2020-10-22 19:46:07 -0400

    refactored table driven tests and added code to calculate area of Triangle

--- structs/shapes.go
+++ structs/shapes.go
@@ -3,7 +3,6 @@ package structs
 import "math"
 
 type Shape interface {
-	Perimeter() float64
 	Area() float64
 }
 
@@ -12,12 +11,12 @@ type Rectangle struct {
 	Height float64
 }
 
-func (r Rectangle) Perimeter() float64 {
-	return 2 * (r.Width + r.Height)
-}
-
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 type Circle struct {
@@ -30,4 +29,13 @@ func (c Circle) Perimeter() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -4,38 +4,38 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 
-	perimeterTests := []struct {
-		shape Shape
-		want  float64
-	}{
-		{Rectangle{10, 10}, 40.0},
-		{Circle{10}, 62.83185307179586},
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
+		want := 40.0
 
-	for _, tt := range perimeterTests {
-		got := tt.shape.Perimeter()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
 		}
-	}
+	})
 
 }
 
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
+		name string
 		shape Shape
-		want  float64
+		hasArea  float64
 	}{
-		{Rectangle{12, 60}, 720.0},
-		{Circle{10}, 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 60}, hasArea: 720.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		// Using tt.name from the case as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 
 }
