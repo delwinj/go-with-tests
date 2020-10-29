255ee89c07e1a34ff94fbbb61e5de24392989d08
Delwin <thekkinkad.d@husky.neu.edu>
2020-10-28 20:59:57 -0400

    determine faster URL using select and httptest)

---
+++ select/racer_test.go
@@ -0,0 +1,47 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("compares speed of servers, returning fastest", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Microsecond)
+		fastServer := makeDelayedServer(0 * time.Microsecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		got, _ := Racer(slowURL, fastURL)
+		want := fastURL
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		time.Sleep(delay)
+		writer.WriteHeader(http.StatusOK)
+	}))
+}
